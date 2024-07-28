d4f8b1bd29df632a80d087e1ef11f685d0f99ebe
Kaiser69 <pen1921@me.com>
2024-07-28 16:05:15 +0200

    Update contactform.js
    change js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,28 +1,39 @@
- document.getElementById('contactForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            
-            const formData = new FormData(this);
-            const data = Object.fromEntries(formData.entries());
+ document
+        .getElementById("contactForm")
+        .addEventListener("submit", function (event) {
+          event.preventDefault();
 
-            fetch('https://formspree.io/f/mrbzpykg', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
+          const formData = new FormData(this);
+          const data = Object.fromEntries(formData.entries());
+
+          fetch("https://formspree.io/f/mrbzpykg", {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.ok) {
+                const confirmationMessage = document.getElementById(
+                  "confirmationMessage"
+                );
+                confirmationMessage.style.display = "block";
+                document.getElementById("contactForm").reset();
+              } else {
+                alert("An error occurred. Please try again.");
+              }
             })
-            .then(response => response.json())
-            .then(data => {
-                if (data.ok) {
-                    document.getElementById('confirmationMessage').style.display = 'block';
-                    document.getElementById('contactForm').reset();
-                } else {
-                    alert('An error occurred. Please try again.');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('An error occurred. Please try again.');
+            .catch((error) => {
+              console.error("Error:", error);
+              alert("An error occurred. Please try again.");
             });
+        });
+
+      document
+        .getElementById("confirmationMessage")
+        .addEventListener("click", function () {
+          this.style.display = "none";
         });
